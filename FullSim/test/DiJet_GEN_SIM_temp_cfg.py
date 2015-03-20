ba2d8e883a7f2b4a262d801875cf90afeecb206f
Kevin Pedro <kpedro88@gmail.com>
2015-03-20 15:36:47 -0500

    add scripts for dijet gensim

---
+++ FullSim/test/DiJet_GEN_SIM_temp_cfg.py
@@ -0,0 +1,116 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.20 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: SinglePiE50HCAL_cfi --conditions auto:upgrade2019 -n 10 --eventcontent FEVTDEBUG -s GEN,SIM --datatier GEN-SIM --beamspot Gauss --customise SLHCUpgradeSimulations/Configuration/combinedCustoms.cust_2019 --geometry Extended2019 --magField 38T_PostLS1 --fileout file:step1.root --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('SIM')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended20YEARReco_cff')
+process.load('Configuration.Geometry.GeometryExtended20YEAR_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedGauss_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(NEVENT)
+)
+
+#random number seed
+process.RandomNumberGeneratorService.generator.initialSeed = cms.untracked.uint32(NEVENT+NPART)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.20 $'),
+    annotation = cms.untracked.string('SinglePiE50HCAL_cfi nevts:10'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+    fileName = cms.untracked.string('file:dijet_gensim_20YEAR_ptENERGYIN_nNEVENT_partNPART.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade20YEAR', '')
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(14000.0),
+    crossSection = cms.untracked.double(175007.8),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        processParameters = cms.vstring('Main:timesAllowErrors = 10000', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tauMax = 10', 
+            'HardQCD:all = on', 
+            'PhaseSpace:pTHatMin = ENERGYIN ', 
+            'PhaseSpace:pTHatMax = ENERGYIN ', 
+            'Tune:pp 5', 
+            'Tune:ee 3'),
+        parameterSets = cms.vstring('processParameters')
+    )
+)
+
+process.genfilter = cms.EDFilter("HEJetsMC",
+  GenTag = cms.untracked.InputTag('ak5GenJets'),
+  MinPt = cms.double(10.)
+)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen*process.genfilter)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.FEVTDEBUGHLToutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.combinedCustoms
+from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_20YEAR
+
+#call to customisation function cust_20YEAR imported from SLHCUpgradeSimulations.Configuration.combinedCustoms
+process = cust_20YEAR(process)
+
+# End of customisation functions
---
+++ FullSim/test/FSsub_gensim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+KEEPTAR=$1
+NUM=500
+ENERGY=30
+JOBDIR=batch
+OUTDIR=root://cmseos.fnal.gov//store/user/pedrok/raddam/gensim
+
+# grid proxy check
+voms-proxy-info --all > /dev/null
+if [ $? -ne 0 ]; then
+  voms-proxy-init -voms cms --valid 168:00
+fi
+
+# tarball of CMSSW area
+if [ -z "$KEEPTAR" ]; then
+  mkdir -p ${JOBDIR}
+  tar --exclude-caches-all -zcf ${JOBDIR}/${CMSSW_VERSION}.tar.gz -C ${CMSSW_BASE}/.. ${CMSSW_VERSION}
+fi
+
+for YEAR in 17 19
+  do
+    for PART in {1..40}
+      do
+        ./FStemp_gensim.sh ${JOBDIR} ${OUTDIR} ${YEAR} ${ENERGY} ${NUM} ${PART}
+      done
+  done
