1a0442a6472789517e5fd8e64c1efe174974edb1
Kevin Pedro <kpedro88@gmail.com>
2015-04-02 14:31:33 -0500

    add option to run cfg's instead of submitting to condor

--- FullSim/test/FStemp.sh
+++ FullSim/test/FStemp.sh
@@ -8,14 +8,12 @@ ENERGY=$5
 LUMI=$6
 NEVENT=$7
 NPART=$8
+JUSTRUN=$9
 
 ILUMI=1E34
 if [ "$LUMI" -gt 500 ]; then
   ILUMI=5E34
 fi
-
-echo ""
-echo ">> `/bin/date` Submitting condor job(s) : $1 $4 $5 $6 $7 $8"
 
 mkdir -p ${JOBDIR}
 
@@ -28,12 +26,22 @@ cat ./${FPRE}_temp_cfg.py \
 | sed -e s/NPART/${NPART}/g \
 > ${JOBDIR}/${FPRE}_20${YEAR}_pt${ENERGY}_lumi${LUMI}_n${NEVENT}_part${NPART}.py
 
-cat ./jobExecCondor.jdl \
-| sed -e s/JOBNAME/${FPRE}_20${YEAR}_pt${ENERGY}_lumi${LUMI}_n${NEVENT}_part${NPART}/g \
-| sed -e s/CMSSWVER/${CMSSW_VERSION}/g \
-| sed -e s~OUTDIR~${OUTDIR}~g \
-> ${JOBDIR}/jobExecCondor_${FPRE}_20${YEAR}_pt${ENERGY}_lumi${LUMI}_n${NEVENT}_part${NPART}.jdl
+if [ -z "$JUSTRUN" ]; then
+  echo ""
+  echo ">> `/bin/date` Submitting condor job : $1 $4 $5 $6 $7 $8"
 
-cd ${JOBDIR}
-condor_submit jobExecCondor_${FPRE}_20${YEAR}_pt${ENERGY}_lumi${LUMI}_n${NEVENT}_part${NPART}.jdl
-cd -
+  cat ./jobExecCondor.jdl \
+  | sed -e s/JOBNAME/${FPRE}_20${YEAR}_pt${ENERGY}_lumi${LUMI}_n${NEVENT}_part${NPART}/g \
+  | sed -e s/CMSSWVER/${CMSSW_VERSION}/g \
+  | sed -e s~OUTDIR~${OUTDIR}~g \
+  > ${JOBDIR}/jobExecCondor_${FPRE}_20${YEAR}_pt${ENERGY}_lumi${LUMI}_n${NEVENT}_part${NPART}.jdl
+  
+  cd ${JOBDIR}
+  condor_submit jobExecCondor_${FPRE}_20${YEAR}_pt${ENERGY}_lumi${LUMI}_n${NEVENT}_part${NPART}.jdl
+  cd -
+else
+  echo ""
+  echo ">> `/bin/date` Running job : $1 $4 $5 $6 $7 $8"
+
+  cmsRun ${JOBDIR}/${FPRE}_20${YEAR}_pt${ENERGY}_lumi${LUMI}_n${NEVENT}_part${NPART}.py
+fi
